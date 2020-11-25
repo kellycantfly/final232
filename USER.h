f34c56ecfb65867f592884da150c84bc71c02a97
Kelly Christensen <christensen.kelly98@gmail.com>
2020-11-25 17:09:55 -0600

    Finished testing

--- USER.h
+++ USER.h
@@ -1,3 +1,6 @@
+#ifndef USER_H
+#define USER_H
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -15,3 +18,5 @@ class USER
 		string getUsername();
 		string getPassword();
 };
+
+#endif
--- driver.cpp
+++ driver.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "USER.h"
 using namespace std;
 
 int main()
