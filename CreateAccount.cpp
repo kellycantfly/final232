f237d4fc33c412f94b259167f90ae59798e25ee0
David Harper <davidgamer789@gmail.com>
2020-11-30 16:50:03 -0600

    created a placeholder file for opening accounts

---
+++ CreateAccount.cpp
@@ -0,0 +1,12 @@
+#include "driver.cpp"
+#include <iostream>
+using namespace std;
+
+void badAccountCreationRequest() { 
+            cout << endl;
+			cout << "######################" << endl;
+			cout << "Only Bank Officials may open an account. Please see a bank official to create an account." << endl;
+            cout << "######################" << endl;
+            cout << endl;
+            main();
+}
