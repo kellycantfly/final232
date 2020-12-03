49e08eaab45b56f8154a243ed541f42c70687709
Kelly Christensen <christensen.kelly98@gmail.com>
2020-12-02 20:31:08 -0600

    function.cpp login features

--- function.cpp
+++ function.cpp
@@ -3,70 +3,80 @@
 #include "function.h"
 using namespace std;
 
-void menu(int menuType) {
+void menu(int menuType, USER use) {
     if (menuType == 1) {
-
+        cout << "Account holder!" << endl;
     }
     if (menuType == 2) {
-
+        cout << "Bank official!" << endl;
+        OFFICIAL::menu();
     }
     if (menuType == 3) {
-
+        cout << "System admin!" << endl;
     }
 }
 
-int validateLogin(string username, string password) {
-    if (username[0] == 'A') {
-        // validate against account holders
-        return 1;
-    }
-    else if (username[0] == 'O') {
-        // validate against offical logins
-        return 2;
+void validateLogin(string username, string password, vector<USER> vec) {
 
+    if (search(username, password, vec) != NULL)
+    {
+        USER* use = search(username, password, vec);
+        string name = use->getUsername();
+        if (name.at(0) == 'O')
+        {
+            cout << "Bank official!" << endl;
+            OFFICAL::use->menu();
+        }
+        else if (name.at(0) == 'A')
+        {
+            cout << "System admin!" << endl;
+        }
+        else if (name.at(0) == 'H')
+        {
+            cout << "Account holder!" << endl;
+        }
     }
-    else if (username[0] == 'S') {
-        //validate against system admins login
-        return 3;
+    else
+    {
+        cout << "No matching account" << endl;
+    }
 
-    }
-    return 0;
 }
 
-void login() {
+void login(vector<USER> vec) {
     string username;
     string password;
     cout << endl;
 	cout << "######################" << endl;
-    cout << "Please enter your credintals to continue." << endl;
+    cout << "Please enter your credentials to continue." << endl;
     cout << "######################" << endl;
     cout << "Username: ";
-    getline(cin,username);
-    cout << endl;
+    cin >> username;
+    cin.clear();
     cout << "Password: ";
-    getline(cin,password);
+    cin >> password;
 
-    switch(validateLogin(username, password)) {
-        case 1: { // account holder
-            menu(1);
-            break;
-        }
-        case 2: { // bank official
-            menu(2);
-            break;
-        }
-        case 3: { // system administrator
-            menu(3);
-            break;
-        }
-        default: {
-            cout << "Invalid credintals..." << endl;
-            login();
-        }
-    }
+    validateLogin(username, password, vec);
+
     
 }
 
+
+USER* search(string username, string password, vector<USER> vec)
+{
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if((vec.at(i).getUsername() == username) && (vec.at(i).getPassword() == password))
+        {
+            return &vec.at(i);
+        }
+        else
+        {
+            cout << "Invalid credentials..." << endl;
+            return NULL;
+        }
+    }
+}
 
 void badAccountCreationRequest() {
     cout << endl;
