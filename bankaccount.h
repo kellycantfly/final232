4e09dc21ffcb78a50cf687c4600158b5d4a7a959
Kelly Christensen <46505064+kellycantfly@users.noreply.github.com>
2020-12-04 23:53:40 -0600

    Update bankaccount.h

--- bankaccount.h
+++ bankaccount.h
@@ -3,46 +3,90 @@
 
 #include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
 
 class BankAccount {
     private:
-        string accID;
-        string userID;
-        double balance;
-        double fee;
+        string dateOpen;
+        string dateClose;
+        string accID; // userID + account type
+        string userID; // matches accountID in User
+        double balance; // TO DO
+        double fee; // TO DO
+        bool status = 0; // 0 is closed, 1 is open
+
     public:
-        void setAccID(string accID) {
 
+        BankAccount()
+        {
+            time_t now = time(0);
+            setDateOpen(ctime(&now));
+            setStatus(1); // account is open
         }
-        void setUserID(string userID) {
 
+        // SETTERS
+        void setUserID(string ID) { userID = ID; }
+        void setAccID(string ID) { accID = ID; }
+        void setDateOpen(string date) { dateOpen = date; }
+        void setDateClose(string date) { dateClose = date; }
+        void setStatus(bool stat) { status = stat; }
+
+        // GETTERS
+        string getUserID() { return userID; }
+        string getAccID() { return accID; }
+        string getDateOpen() { return dateOpen; }
+        string getDateClose() { return dateClose; }
+        bool getStatus() { return status; }
+
+        // Prints the accID and userID. Can be changed
+        void print()
+        {
+            cout << "AccID: " << getAccID() << " User ID: " << getUserID() << endl;
         }
 };
 
 class SavingAccount : public BankAccount {
     private:
-        double interestRate;
+        double interestRate; // TO DO
+
     public:
-        void setInterestRate(double intRate) {
-            interestRate = intRate;
+        SavingAccount(string accountID)
+        {
+            setUserID(accountID);
+            setAccID(accountID.insert(0, "S")); // adds S identifer to accID
         }
+
+        // SETTERS
+        void setInterestRate(double intRate) { interestRate = intRate; }
 
 
 };
 
 class CheckingAccount : public BankAccount {
 
+    public:
+        CheckingAccount(string accountID)
+        {
+            setUserID(accountID);
+            setAccID(accountID.insert(0, "C")); // adds C identifer to accID
+        }
 };
 
 class CDAccount : public BankAccount {
     private:
-        string creationDate;
+
+        // help with time functionality
         int termLength;
-        string dateOpen;
-        string dateClose;
         int maturity;
-        int interestRate;
+        int interestRate; // TO DO
+
+        public:
+            CDAccount(string accountID)
+            {
+                setUserID(accountID);
+                setAccID(accountID.insert(0, "D")); // adds D identifer to account
+            }
 
 };
 
