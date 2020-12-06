79ef61e771da5c4b59ae4951852d6ed2a17b267f
David <Davidgamer789@gmail.com>
2020-12-05 21:04:57 -0600

    Added functionality to clientMenu

--- clientMenu.cpp
+++ clientMenu.cpp
@@ -8,7 +8,7 @@
 #include <time.h>
 using namespace std;
 
-void clientMenu(accountHolder currentUser, vector<official> &officials, vector<admin> &admins, vector<accountHolder> &clients) {
+void clientMenu(accountHolder currentUser, vector<official> &officials, vector<admin> &admins, vector<accountHolder> &clients, financeAccounts &acc) {
     loadUsers(officials,admins,clients);
 
     cout << "##############" << endl;
@@ -26,10 +26,68 @@ void clientMenu(accountHolder currentUser, vector<official> &officials, vector<a
     cout << " [3] CLOSE ACCOUNT " << endl;
     cout << " [3] LOOK AT TRANSCATION LOGS " << endl;
     cout << " [4] CHANGE PASSWORD " << endl;
-    cout << " [5] LOG-OUT " << endl; 
+    cout << " [5] CHECK BALANCE "<< endl;
+    cout << " [6] LOG-OUT " << endl; 
     cout << "##############" << endl;
     string buffer = "";
     getline(cin, buffer);
     int userSelection = stoi(buffer);
+    switch(userSelection) {
+        case 1: {
+            cout << "Which account would you like to deposit funds into: " << endl;
+            string account;
+            getline(cin,account);
+            if(account[0] == 'C') {
+                for(int i = 0; i < acc.checkings.size(); i++) {
+                    if(account == acc.checkings[i].getAccountID() && currentUser.getUserID() == acc.checkings[i].getUserID()) {
+                        cout << "Current Balance: $"+ to_string(acc.checkings[i].getBalance()) << endl;
+                        cout << "How much would you like to deposit?" << endl;
+                        cout << "$";
+                        string amount;
+                        getline(cin, amount);
+                        double amt = stod(amount);
+                        acc.checkings[i].deposit(amt);
+                        cout << endl;
+                        cout << "Your new balance is: $" + to_string(acc.checkings[i].getBalance());
+                        clientMenu(currentUser, officials, admins, clients, acc);
+                    }
+                }
+            }else if(account[0] == 'F') {
+                for(int i = 0; i < acc.savings.size(); i++) {
+                    if(account == acc.savings[i].getAccountID() && currentUser.getUserID() == acc.savings[i].getUserID()) {
+                        cout << "Current Balance: $"+ to_string(acc.savings[i].getBalance());
+                        cout << "How much would you like to deposit?" << endl;
+                        cout << "$";
+                        string amount;
+                        getline(cin, amount);
+                        double amt = stod(amount);
+                        acc.savings[i].deposit(amt);
+                        cout << "Your new balance is: $" + to_string(acc.savings[i].getBalance());
+                        clientMenu(currentUser, officials, admins, clients, acc);
+                    }
+                }
 
+            }else if(account[0] == 'D') {
+                cout << "You are not able to deposit money into a fixed term CD." << endl;
+            }
+            clientMenu(currentUser, officials, admins, clients, acc);
+        }
+        case 2: {
+
+        }
+        case 3: {
+            cout << "Coming Soon!" << endl;
+        }
+        case 4: {
+            cout << "Enter New Password: " << endl;
+            string password;
+            getline(cin, password);
+            currentUser.setPassword(password);
+            cout << "Password changed" << endl;
+            clientMenu(currentUser, officials, admins, clients, acc);
+        }
+        case 5: {
+            main();
+        }
+    }
 }
